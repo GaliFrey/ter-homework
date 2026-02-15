e6a5686b04862d49ddd4190247db5927fcd8d5b7
Zagoskin Vladislav <zagoskin.vladislav.a@yandex.ru>
2026-02-15 12:04:57 +0300

    block 02 - task 2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -3,11 +3,13 @@
 
 variable "cloud_id" {
   type        = string
+  default     = "b1gseibtsnsdce94kftf"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1gjtgthu82vhe79q1uv"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -35,4 +37,59 @@ variable "vms_ssh_root_key" {
   type        = string
   default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN5yIIHkmPmdqtgG0rNRrnLZ2L1ebC9pmu66PvcBdpP5 heimdall@Heimdall"
   description = "ssh-keygen -t ed25519"
+}
+
+### vm_web
+
+variable "vm_web_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Идентификатор образа"
+}
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "Имя ВМ"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "Идентификатор платформы"
+}
+
+variable "vm_web_cores" {
+  type = number
+  default = 2
+  description = "vCPU ВМ"
+}
+variable "vm_web_memory" {
+  type = number
+  default = 2
+  description = "RAM ВМ"
+}
+
+variable "vm_web_core_frac" {
+  type = number
+  default = 20
+  description = "Гарантированная доля vCPU ВМ"
+}
+
+variable "vm_web_preemptible" {
+  type = bool
+  default = true
+  description = "Прерываемая ВМ"
+}
+
+variable "vm_web_network_nat" {
+  type = bool
+  default = true
+  description = "Использовать NAT"
+}
+
+variable "vm_web_serial_port_enable" {
+  type = number
+  default = 1
+  description = "Активировать серийный порт"
 }
