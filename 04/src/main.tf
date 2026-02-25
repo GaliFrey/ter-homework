5aac22e8c1ebda137684db3d1bbcf5ff48980d7e
Zagoskin Vladislav <zagoskin.vladislav.a@yandex.ru>
2026-02-25 15:19:54 +0300

    block 04 - task 1

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -8,3 +8,60 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+data "template_file" "cloud_init_marketing" {
+  template = file("${path.module}/cloud-init.yml")
+
+  vars = {
+    ssh_public_key = file(pathexpand(var.ssh_key_path))
+  }
+}
+
+data "template_file" "cloud_init_analytics" {
+  template = file("${path.module}/cloud-init.yml")
+
+  vars = {
+    ssh_public_key = file(pathexpand(var.ssh_key_path))
+  }
+}
+
+module "marketing_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "marketing"
+  instance_count = 1
+  image_family   = var.vm_image_family
+  public_ip      = true
+
+  labels = { 
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloud_init_marketing.rendered
+    serial-port-enable = 1
+  }
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "analytics"
+  instance_count = 1
+  image_family   = var.vm_image_family
+  public_ip      = true
+
+  labels = { 
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloud_init_analytics.rendered
+    serial-port-enable = 1
+  }
+}
